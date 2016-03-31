82cb591218ceabc277c35d0d78cf980fdd47fccd
encse <encse@csokavar.hu>
2016-03-31 22:58:47 +0200

    UxRom

--- Nes/app/mapper/MemoryMapperFactory.ts
+++ Nes/app/mapper/MemoryMapperFactory.ts
@@ -6,6 +6,8 @@
                 return new Mmc0(nesImage);
             case 1:
                 return new Mmc1(nesImage);
+            case 2:
+                return new UxRom(nesImage);
             case 4:
                 return new Mmc3(nesImage);
             default:
--- Nes/wwwroot/js/app.js
+++ Nes/wwwroot/js/app.js
@@ -650,6 +650,8 @@ var Most6502Base = (function () {
             }
             this.ipCur = this.ip;
             //this.trace(this.opcode);
+            //if (this.ipCur === 0xe3e8 || this.ipCur === 0xe3fb)
+            //    console.log(this.ipCur.toString(16));
             this.addr = this.addrHi = this.addrLo = this.addrPtr = this.ptrLo = this.ptrHi = this.ipC = this.addrC = 0;
         }
         this.opcodes[this.opcode].call(this);
@@ -4723,13 +4725,12 @@ var Most6502Base = (function () {
     /* BRK BRK 7 */
     Most6502Base.prototype.op0x0 = function () {
         switch (this.t) {
-            case 0:
-                {
-                    if (this.enablePCIncrement)
-                        this.ip++;
-                    this.t++;
-                    break;
-                }
+            case 0: {
+                if (this.enablePCIncrement)
+                    this.ip++;
+                this.t++;
+                break;
+            }
             case 1: {
                 this.memory.getByte(this.ip);
                 if (this.enablePCIncrement)
@@ -10630,6 +10631,8 @@ var MemoryMapperFactory = (function () {
                 return new Mmc0(nesImage);
             case 1:
                 return new Mmc1(nesImage);
+            case 2:
+                return new UxRom(nesImage);
             case 4:
                 return new Mmc3(nesImage);
             default:
@@ -11119,6 +11122,63 @@ var Mmc3 = (function () {
         this.prevA12 = this.curA12;
     };
     return Mmc3;
+})();
+///<reference path="IMemoryMapper.ts"/>
+var UxRom = (function () {
+    function UxRom(nesImage) {
+        this.PRGBanks = this.splitMemory(nesImage.ROMBanks, 0x4000);
+        this.CHRBanks = [];
+        while (this.PRGBanks.length < 2)
+            this.PRGBanks.push(new Ram(0x4000));
+        while (this.CHRBanks.length < 2)
+            this.CHRBanks.push(new Ram(0x1000));
+        this.memory = new CompoundMemory(new CleverRam(0x800, 4), new Ram(0x2000), new Ram(0x4000), this.PRGBanks[0], this.PRGBanks[this.PRGBanks.length - 1]);
+        this.nametableA = new Ram(0x400);
+        this.nametableB = new Ram(0x400);
+        this.nametable = new CompoundMemory(this.nametableA, this.nametableB, this.nametableA, this.nametableB);
+        if (nesImage.fVerticalMirroring) {
+            this.nametable.rgmemory[0] = this.nametable.rgmemory[2] = this.nametableA;
+            this.nametable.rgmemory[1] = this.nametable.rgmemory[3] = this.nametableB;
+        }
+        else {
+            this.nametable.rgmemory[0] = this.nametable.rgmemory[1] = this.nametableA;
+            this.nametable.rgmemory[2] = this.nametable.rgmemory[3] = this.nametableB;
+        }
+        this.vmemory = new CompoundMemory(this.CHRBanks[0], this.CHRBanks[1], this.nametable, new Ram(0x1000));
+        this.memory.shadowSetter(0x8000, 0xffff, this.setByte.bind(this));
+    }
+    UxRom.prototype.setByte = function (addr, value) {
+        /*7  bit  0
+         ---- ----
+         xxxx pPPP
+              ||||
+              ++++- Select 16 KB PRG ROM bank for CPU $8000-$BFFF
+          (UNROM uses bits 2-0; UOROM uses bits 3-0)
+         
+         Emulator implementations of iNES mapper 2 treat this as a full 8-bit bank select register, without bus conflicts. This allows the mapper to be used for similar boards that are compatible.
+         To make use of all 8-bits for a 4 MB PRG ROM, an NES 2.0 header must be used (iNES can only effectively go to 2 MB).
+         The original UxROM boards used by Nintendo were subject to bus conflicts, but the relevant games do not rely on this.
+         */
+        this.memory.rgmemory[3] = this.PRGBanks[value & 0xff];
+    };
+    UxRom.prototype.splitMemory = function (romBanks, size) {
+        var result = [];
+        for (var _i = 0; _i < romBanks.length; _i++) {
+            var rom = romBanks[_i];
+            var i = 0;
+            if (rom.size() % size)
+                throw 'cannot split memory';
+            while (i < rom.size()) {
+                result.push(rom.subArray(i, size));
+                i += size;
+            }
+        }
+        return result;
+    };
+    UxRom.prototype.setCpuAndPpu = function (cpu) {
+    };
+    UxRom.prototype.clk = function () { };
+    return UxRom;
 })();
 ///<reference path="Memory.ts"/>
 var CleverRam = (function () {
